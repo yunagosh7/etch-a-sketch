78686c20f084142494d7eeb3a8014d7c40e9e046
Vila Juan Cruz <yunagosh7@gmail.com>
2022-10-20 20:53:03 -0300

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,34 @@
 const container = document.getElementById("container");
-const fragment = document.createDocumentFragment();
+const btn = document.getElementById("btn")
+
 let divs = [];
 
-
-for(let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.classList = "grid-item" 
-    divs.push(div)
-    fragment.appendChild(div);
+const createGrid = n =>{
+    let fragment = document.createDocumentFragment();
+    let num = 0;
+    num = n * n 
+    for(let i = 0; i < num; i++){
+        const div = document.createElement("div");
+        div.classList = "grid-item";
+        container.style.gridTemplateColumns = `repeat(${n},1fr)`
+        container.style.gridTemplateRows = `repeat(${n},1fr)`
+        divs.push(div)
+        fragment.appendChild(div);
+   }
+   container.appendChild(fragment)
 }
 
-divs.forEach(div=>{
-    div.addEventListener("mouseover",()=>{
-        div.style.backgroundColor = "#e55"
+btn.addEventListener("click",()=>{
+    let n = 0;
+    n = prompt("num");
+    n = parseInt(n)
+    createGrid(n);
+    divs.forEach((div)=>{
+        div.addEventListener("mouseover",()=>{
+            div.style.backgroundColor = "#e55"
+        })
     })
 })
 
 
-container.appendChild(fragment)
+
