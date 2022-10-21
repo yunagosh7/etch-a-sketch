b3b761314c195bf08fb60dc1295e579be5496ff6
Vila Juan Cruz <yunagosh7@gmail.com>
2022-10-21 19:02:44 -0300

    Añadi las funciones principales

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,8 @@ container.id = "container";
 const displayGrid = ()=>{
     const fragment = document.createDocumentFragment();
     container.style.display = "grid";
-    container.style.gridTemplateColumns = "16"
-    container.style.gridTemplateRows = "16"
+    container.style.gridTemplateColumns = "repeat(16, 1fr)"
+    container.style.gridTemplateRows = "repeat(16,1fr)"
 for(let i = 0; i < 256;i++){
     const div = document.createElement("div");
     div.classList = "grid-item";
@@ -23,7 +23,7 @@ displayGrid()
 
 const createGrid = n =>{
     divs = [];
-    main.removeChild(container)
+    const containerToRemove = document.getElementById("container")
     const container = document.createElement("div");
     container.id = "container";
     const fragment = document.createDocumentFragment();
@@ -36,10 +36,9 @@ const createGrid = n =>{
         container.style.gridTemplateRows = `repeat(${n},1fr)`
         divs.push(div)
         fragment.appendChild(div);
-   }
-   
-   container.appendChild(fragment)
-    main.appendChild(container)
+   }   
+    container.appendChild(fragment)
+    main.replaceChild(container, containerToRemove)
 }
 
 
