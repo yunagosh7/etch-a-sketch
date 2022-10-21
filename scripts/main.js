7b92bc5912d8d1d37871a72f09bb9af13f01c83e
Vila Juan Cruz <yunagosh7@gmail.com>
2022-10-21 18:48:11 -0300

    Resolviendo bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,32 @@
-const container = document.getElementById("container");
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
+const main = document.getElementById("main")
 
-let divs = [];
+let divs = []
+
+const container = document.createElement("div");
+container.id = "container";
+const displayGrid = ()=>{
+    const fragment = document.createDocumentFragment();
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = "16"
+    container.style.gridTemplateRows = "16"
+for(let i = 0; i < 256;i++){
+    const div = document.createElement("div");
+    div.classList = "grid-item";
+    fragment.appendChild(div);
+    }
+    container.appendChild(fragment)
+    main.appendChild(container)
+}
+
+displayGrid()
 
 const createGrid = n =>{
-    let fragment = document.createDocumentFragment();
+    divs = [];
+    main.removeChild(container)
+    const container = document.createElement("div");
+    container.id = "container";
+    const fragment = document.createDocumentFragment();
     let num = 0;
     num = n * n 
     for(let i = 0; i < num; i++){
@@ -15,20 +37,27 @@ const createGrid = n =>{
         divs.push(div)
         fragment.appendChild(div);
    }
+   
    container.appendChild(fragment)
+    main.appendChild(container)
 }
+
+
+    
+    
 
 btn.addEventListener("click",()=>{
     let n = 0;
     n = prompt("num");
     n = parseInt(n)
     createGrid(n);
-    divs.forEach((div)=>{
-        div.addEventListener("mouseover",()=>{
-            div.style.backgroundColor = "#e55"
+    divs.forEach(div=>{
+        div.addEventListener("click",()=>{
+            div.style.backgroundColor = "red"
         })
     })
-})
+    })
+
 
 
 
